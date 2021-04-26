2b9253f89d38aa5ed59731209ee91f05f74a2889
Josh Stoik <josh@blobfolio.com>
2021-04-25 20:57:18 -0700

    fix: enable tiling

--- refract_core/src/avif.rs
+++ refract_core/src/avif.rs
@@ -8,6 +8,7 @@ use crate::{
 };
 use libavif_sys::{
 	AVIF_CHROMA_UPSAMPLING_BILINEAR,
+	AVIF_CODEC_CHOICE_RAV1E,
 	AVIF_PLANES_YUV,
 	AVIF_RESULT_OK,
 	AVIF_RGB_FORMAT_RGBA,
@@ -58,7 +59,7 @@ impl TryFrom<&TreatedSource> for AvifImage {
 			depth: 8,
 			format: AVIF_RGB_FORMAT_RGBA,
 			chromaUpsampling: AVIF_CHROMA_UPSAMPLING_BILINEAR,
-			ignoreAlpha: 0,
+			ignoreAlpha: ! src.color().has_alpha() as _,
 			alphaPremultiplied: 0,
 			pixels: raw.as_ptr() as *mut u8,
 			rowBytes: 4 * width,
@@ -94,17 +95,22 @@ impl Drop for AvifImage {
 /// resources on drop, but also handles setup.
 struct AvifEncoder(*mut avifEncoder);
 
-impl TryFrom<NonZeroU8> for AvifEncoder {
-	type Error = RefractError;
-
-	fn try_from(quality: NonZeroU8) -> Result<Self, RefractError> {
+impl AvifEncoder {
+	#[allow(clippy::cast_possible_truncation)] // It fits.
+	#[allow(clippy::cast_possible_wrap)]
+	/// # New Instance.
+	fn new(width: usize, height: usize, quality: NonZeroU8) -> Result<Self, RefractError> {
 		// Convert quality to quantizers. AVIF is so convoluted...
 		let (q, aq) = quality_to_quantizers(quality);
-		let threads = i32::try_from(num_cpus::get())
-				.map_err(|_| RefractError::Encode)?;
 
+		// Total threads.
+		let cpus = num_cpus::get();
+		let threads = i32::try_from(cpus).map_err(|_| RefractError::Encode)?;
+
+		// Start up the encoder!
 		let encoder = unsafe { avifEncoderCreate() };
 		unsafe {
+			(*encoder).codecChoice = AVIF_CODEC_CHOICE_RAV1E;
 			(*encoder).maxThreads = threads;
 
 			(*encoder).minQuantizer = i32::from(q);
@@ -116,6 +122,38 @@ impl TryFrom<NonZeroU8> for AvifEncoder {
 			// There is a speed 0, but it is brutally slow and has very little
 			// benefit.
 			(*encoder).speed = 1;
+
+			// Enable tiling if we are multi-threaded. We want to try to keep
+			// the combined X/Y value equal to the total number of threads,
+			// while also ensuring we aren't trying to divide a small dimension
+			// into too many chunks.
+			if cpus > 1 {
+				let tiles_x;
+				let tiles_y;
+
+				// Prioritize carving up width.
+				if width >= height {
+					// The magic "128" number is 2^6, where 6 is the absolute
+					// maximum accepted tiling value.
+					tiles_x = cpus.min(num_integer::div_floor(width, 128)).max(1);
+					tiles_y = num_integer::div_floor(cpus, tiles_x)
+						.min(num_integer::div_floor(height, 128))
+						.max(1);
+				}
+				// Prioritize carving up the height.
+				else {
+					tiles_y = cpus.min(num_integer::div_floor(height, 128)).max(1);
+					tiles_x = num_integer::div_floor(cpus, tiles_y)
+						.min(num_integer::div_floor(width, 128))
+						.max(1);
+				}
+
+				// We can only split up to 6 times, so cap values thusly.
+				if tiles_x > 1 || tiles_y > 1 {
+					(*encoder).tileRowsLog2 = 6.min(tiles_x) as i32;
+					(*encoder).tileColsLog2 = 6.min(tiles_y) as i32;
+				}
+			}
 		};
 
 		Ok(Self(encoder))
@@ -158,7 +196,8 @@ impl Drop for AvifData {
 /// encountered during encoding or saving.
 pub(super) fn make_lossy(img: &TreatedSource, quality: NonZeroU8) -> Result<Vec<u8>, RefractError> {
 	let image = AvifImage::try_from(img)?;
-	let encoder = AvifEncoder::try_from(quality)?;
+	let (width, height) = img.dimensions();
+	let encoder = AvifEncoder::new(width, height, quality)?;
 	let mut data = AvifData(avifRWData::default());
 
 	// Encode!
