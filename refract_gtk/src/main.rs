d82742f39314c9a8410bbdd4ad63a46e5cb536c8
Josh Stoik <josh@blobfolio.com>
2021-06-06 12:54:49 -0700

    cleanup: refactor bindings a bit

--- refract_gtk/src/main.rs
+++ refract_gtk/src/main.rs
@@ -84,6 +84,7 @@ fn _main() -> Result<(), RefractError> {
 	application.connect_activate(|app| {
 		let window = Arc::new(Window::try_from(app).expect("Unable to build GTK window."));
 		setup_ui(&window);
+		window.paint();
 	});
 
 	application.run(&[]);
@@ -105,24 +106,10 @@ fn init_resources() -> Result<(), RefractError> {
 #[allow(clippy::similar_names)] // We're being consistent.
 /// # Setup UI.
 ///
-/// This finishes the UI setup, hooking up communication channels, event
-/// bindings, etc.
+/// This finishes the UI setup, hooking up communication channels, and event
+/// bindings requiring those channels (or the [`Window`] as a whole).
 fn setup_ui(window: &Arc<Window>) {
 	let (stx, mtx, srx) = Share::init(Arc::clone(window));
-
-	// The encoder checkbox settings.
-	{
-		macro_rules! chk_cb {
-			($validate_cb:ident, $($btn:expr),+) => {$(
-				let wnd2 = Arc::clone(window);
-				$btn.connect_toggled(move |btn| {
-					if ! wnd2.$validate_cb() { btn.set_active(true); }
-				});
-			)+};
-		}
-		chk_cb!(has_encoders, window.chk_avif, window.chk_jxl, window.chk_webp);
-		chk_cb!(has_modes, window.chk_lossy, window.chk_lossless);
-	}
 
 	// The toggle.
 	{
@@ -136,37 +123,18 @@ fn setup_ui(window: &Arc<Window>) {
 	// Discard/Keep button.
 	{
 		macro_rules! feedback_cb {
-			($btn:expr, $status:expr) => {
+			($(($btn:ident, $status:ident)),+) => ($(
 				let mtx2 = mtx.clone();
 				let wnd2 = Arc::clone(window);
-				$btn.connect_clicked(move |_| {
+				window.$btn.connect_clicked(move |_| {
 					wnd2.remove_candidate();
 					wnd2.paint();
-					mtx2.send($status).unwrap();
+					mtx2.send(ShareFeedback::$status).unwrap();
 				});
-			};
+			)+);
 		}
 
-		feedback_cb!(window.btn_discard, ShareFeedback::Discard);
-		feedback_cb!(window.btn_keep, ShareFeedback::Keep);
-	}
-
-	// The quit button.
-	{
-		let wnd2 = Arc::clone(window);
-		window.mnu_quit.connect_activate(move |_| { wnd2.wnd_main.close(); });
-	}
-
-	// About.
-	{
-		let wnd2 = Arc::clone(window);
-		window.mnu_about.connect_activate(move |_| {
-			if let Ok(about) = wnd2.about() {
-				if gtk::ResponseType::None != about.run() {
-					about.emit_close();
-				}
-			}
-		});
+		feedback_cb!((btn_discard, Discard), (btn_keep, Keep));
 	}
 
 	// Add a file!
@@ -187,35 +155,4 @@ fn setup_ui(window: &Arc<Window>) {
 			if wnd2.maybe_add_directory() { wnd2.encode(&stx, &srx); }
 		});
 	}
-
-	// Sync display of ab/format/quality fields with `lbl_quality`.
-	{
-		macro_rules! preview_cb {
-			($hook:ident, $action:ident, $opacity:literal) => {
-				let wnd2 = Arc::clone(window);
-				window.lbl_quality.$hook(move |_| {
-					wnd2.box_ab.set_opacity($opacity);
-					wnd2.lbl_format.set_opacity($opacity);
-					wnd2.lbl_format_val.$action();
-					wnd2.lbl_quality_val.$action();
-				});
-			};
-		}
-
-		preview_cb!(connect_show, show, 1.0);
-		preview_cb!(connect_hide, hide, 0.0);
-	}
-
-	// Keep the status log scrolled to the end.
-	{
-		let wnd2 = Arc::clone(window);
-		window.lbl_status.connect_size_allocate(move |_, _| {
-			if let Some(adj) = wnd2.wnd_status.get_vadjustment() {
-				adj.set_value(adj.get_upper());
-			}
-		});
-	}
-
-	// Give it one final paint!
-	window.paint();
 }
